7b41b26e6f09f7c3831eb51b6beee815651175f0
Rocky Chowdhury <rocky20809@gmail.com>
2024-04-30 05:21:13 +0600

    quesryset added

--- Momentscape/views.py
+++ Momentscape/views.py
@@ -6,4 +6,11 @@ from . import serializers
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
-    
+    def get_queryset(self):
+        queryset = super().get_queryset() 
+        print(self.request.query_params)
+        user_id = self.request.query_params.get('user_id')
+        
+        if user_id:
+            queryset = queryset.filter(id=user_id)
+        return queryset
